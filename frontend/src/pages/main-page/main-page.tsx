07a90dc25e31030263e45776de48e7ca74432afb
Gantelia <helengriva@gmail.com>
2022-12-29 21:49:10 +0300

    Подключит шрифты

--- frontend/src/pages/main-page/main-page.tsx
+++ frontend/src/pages/main-page/main-page.tsx
@@ -4,6 +4,7 @@ function MainPage() {
   return (
     <main className="main">
       <h1 className="visually-hidden">Синтез и распознавание калмыцкой речи</h1>
+      <button className="tab-button"></button>
     </main>
   );
 }
